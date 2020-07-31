2d2262b1783d8c4295456ba3e2904f180e7bbdb3
MateuszRostkowski <mateusz.rostkowsky995@gmail.com>
2020-07-31 20:58:36 +0200

    add error component

---
+++ src/components/Error.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Error = () => {
+  return (
+    <div className="error-container">
+        <h1>Nie znaleźono notatki</h1>
+    </div>
+  );
+};
+
+export default Error;
