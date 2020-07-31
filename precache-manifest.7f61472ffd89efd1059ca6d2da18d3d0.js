57df5072f48c240ebaa6eac8fe6ace4c1eda5d0c
MateuszRostkowski <mateusz.rostkowsky995@gmail.com>
2020-07-31 22:17:43 +0200

    Updates

---
+++ precache-manifest.7f61472ffd89efd1059ca6d2da18d3d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b06ebe93e415a40526bcbd8e929f6114",
+    "url": "/notes/index.html"
+  },
+  {
+    "revision": "20f8ead57e9d97bb8174",
+    "url": "/notes/static/css/2.ff29e27e.chunk.css"
+  },
+  {
+    "revision": "268510071ce3d0fc798c",
+    "url": "/notes/static/css/main.97a3fc83.chunk.css"
+  },
+  {
+    "revision": "20f8ead57e9d97bb8174",
+    "url": "/notes/static/js/2.d12bcb43.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/notes/static/js/2.d12bcb43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "268510071ce3d0fc798c",
+    "url": "/notes/static/js/main.ecf78922.chunk.js"
+  },
+  {
+    "revision": "2579272ee6d414dcade9",
+    "url": "/notes/static/js/runtime-main.5c1acd6b.js"
+  }
+]);
