2b39a7c81dc637cb56b6b84ad1020c8d7be5906a
MateuszRostkowski <mateusz.rostkowsky995@gmail.com>
2020-08-10 18:17:51 +0200

    move add note to different component

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,63 +1,8 @@
-import React, { FC, useState, useEffect } from "react";
-import { Link, useHistory } from "react-router-dom";
-import getNotesList, { NOTE_LIST_KEY } from "../helpers/getNotesList";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import getNotesList from "../helpers/getNotesList";
 import { ListNoteItem } from "../helpers/interfaces";
-import { TYPING_MODE_KEY } from "./NoteContainer";
-import { Modal } from "./Modal";
-
-interface AddNoteProps {
-  toggleMode: () => void;
-}
-
-const AddNote: FC<AddNoteProps> = ({ toggleMode }) => {
-  const { push } = useHistory()
-  const [name, setName] = useState("")
-
-  const handleAddNote = () => {
-    if(!name) {
-      toggleMode()
-      return 
-    }
-
-    if(name === NOTE_LIST_KEY || name === TYPING_MODE_KEY) {
-      alert("this name is not allowed")
-      return
-    }
-    const list = getNotesList()
-    const newNote: ListNoteItem = {
-      name
-    }
-    const newList = [newNote, ...list]
-    localStorage.setItem(name, JSON.stringify({
-      name,
-      value: ""
-    }))
-    localStorage.setItem(NOTE_LIST_KEY, JSON.stringify(newList))
-    push(`/${name}`)
-    toggleMode()
-  }
-
-  return (
-    <Modal>
-      <div className="add-note-wrappper">
-        <div className="add-note-container">
-          <a
-            href="/"
-            onClick={(e) => {
-              e.preventDefault();
-              toggleMode();
-            }}
-            >
-            Close
-          </a>
-          <input value={name} onChange={(e) => setName(e.target.value)}/>
-          <button onClick={handleAddNote}>Add new note</button>
-        </div>
-        <div onClick={toggleMode} className="add-note-background"></div>
-      </div>
-    </Modal>
-  );
-};
+import AddNote from "./AddNote";
 
 function SideBar() {
   const [addMode, setAddMode] = useState(false);
