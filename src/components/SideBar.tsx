85ddbb0878ce4a8b66e5610f562c96d4fa7a0aaa
MateuszRostkowski <mateusz.rostkowsky995@gmail.com>
2020-08-10 19:12:45 +0200

    animate sidebar container width

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -3,45 +3,58 @@ import { Link } from "react-router-dom";
 import getNotesList from "../helpers/getNotesList";
 import { ListNoteItem } from "../helpers/interfaces";
 import AddNote from "./AddNote";
+import { CSSTransition } from "react-transition-group";
 
 function SideBar() {
   const [addMode, setAddMode] = useState(false);
-  const [notesList, setNotesList] = useState([])
+  const [notesList, setNotesList] = useState([]);
   const [showNotesList, setShowNotesList] = useState(false);
 
   useEffect(() => {
-    const list = getNotesList()
-    setNotesList(list)
-  }, [addMode])
+    const list = getNotesList();
+    setNotesList(list);
+  }, [addMode]);
 
-  
   const toggleMode = () => setAddMode(!addMode);
   return (
     <>
-      <div className="sidebar-toggle" onClick={() => setShowNotesList(!showNotesList)}>
+      <div
+        className="sidebar-toggle"
+        onClick={() => setShowNotesList(!showNotesList)}
+      >
         <span className="burger-line burger-line-1"></span>
         <span className="burger-line burger-line-1"></span>
         <span className="burger-line burger-line-1"></span>
       </div>
-      <div className={`sidebar-container${showNotesList ? " sidebar-container--hidden" : ""}`}>
-        <a
-          href="/"
-          onClick={(e) => {
-            e.preventDefault();
-            toggleMode();
-          }}
-        >
-          Dodaj notatkę
-        </a>
-        <h1>Notes app</h1>
-        <div className="links-container">
-          {notesList.map((item: ListNoteItem, index: number) => {
-            return (
-              <Link key={index} to={`/${item.name}`}>{item.name}</Link>
-            )
-          })}
+      <CSSTransition
+        in={showNotesList}
+        timeout={200}
+        classNames="sidebar-wrapper"
+      >
+        <div className="sidebar-wrapper">
+          <div className="sidebar-container">
+            <a
+              href="/"
+              onClick={(e) => {
+                e.preventDefault();
+                toggleMode();
+              }}
+            >
+              Dodaj notatkę
+            </a>
+            <h1>Notes app</h1>
+            <div className="links-container">
+              {notesList.map((item: ListNoteItem, index: number) => {
+                return (
+                  <Link key={index} to={`/${item.name}`}>
+                    {item.name}
+                  </Link>
+                );
+              })}
+            </div>
+          </div>
         </div>
-      </div>
+      </CSSTransition>
       {addMode && <AddNote toggleMode={toggleMode} />}
     </>
   );
