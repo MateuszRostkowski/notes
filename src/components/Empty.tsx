570be5369aa12aa0e5a39adf821242069de6c67a
MateuszRostkowski <mateusz.rostkowsky995@gmail.com>
2020-11-17 22:31:19 +0100

    change polish to english

--- src/components/Empty.tsx
+++ src/components/Empty.tsx
@@ -1,18 +1,20 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 
 const Empty = () => {
-  const [shown, setShown] = useState(false)
+  const [shown, setShown] = useState(false);
 
   useEffect(() => {
     setTimeout(() => {
-      setShown(true)
-    }, 1000)
-  }, [])
+      setShown(true);
+    }, 1000);
+  }, []);
 
   return (
     <div className="empty-container">
-        <h1>Nie masz jeszcze żadnej notatki</h1>
-        <p className={`add-new-info${shown ? " add-new-info--shown" : ""}`}>← Wciśnij tutaj aby dodać nową notatkę</p>
+      <h1>You don't have any note</h1>
+      <p className={`add-new-info${shown ? ' add-new-info--shown' : ''}`}>
+        ← Click here to add one
+      </p>
     </div>
   );
 };
