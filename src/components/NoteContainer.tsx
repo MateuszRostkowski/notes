f33eb036c80b87177d06356b2ea9f009b8b91eed
MateuszRostkowski <mateusz.rostkowsky995@gmail.com>
2021-02-09 10:09:11 +0100

    make the markdown checkbox dynamic

--- src/components/NoteContainer.tsx
+++ src/components/NoteContainer.tsx
@@ -1,6 +1,7 @@
 import React, { FC, useState, useEffect } from 'react';
 import { Controlled as CodeMirror } from 'react-codemirror2';
 import ReactMarkdown from 'react-markdown/with-html';
+
 import CodeBlock from './CodeBlock';
 import NoteSettings from './NoteSettings';
 
@@ -31,6 +32,32 @@ const options = {
     'Cmd-B': (cm: any) => generateKeySelection(cm, '**'),
     'Cmd-I': (cm: any) => generateKeySelection(cm, '*'),
   },
+};
+
+const markdownChecked = '- [x]';
+const markdownUnchecked = '- [ ]';
+
+const WrapCheckBox = (props: any) => {
+  const { markdown, setMarkdown, sourcePosition, checked, children } = props;
+  const flipCheckBox = () => {
+    try {
+      const lineIndex = sourcePosition.start.line - 1;
+      const lines = markdown.split('\n');
+      const find = checked ? markdownChecked : markdownUnchecked;
+      const replace = checked ? markdownUnchecked : markdownChecked;
+      console.log({ lineIndex, lines, find, replace });
+      lines[lineIndex] = lines[lineIndex].replace(find, replace);
+      setMarkdown(lines.join('\n'));
+    } catch (error) {
+      console.warn('Error while filpin checkbox: ', error);
+    }
+  };
+  return (
+    <li>
+      <input type="checkbox" checked={checked} onClick={flipCheckBox} />
+      {children}
+    </li>
+  );
 };
 
 const NoteContainer: FC<Props> = ({ noteId }) => {
@@ -71,6 +98,26 @@ const NoteContainer: FC<Props> = ({ noteId }) => {
       );
     }
   }, [value, noteId, noteName]);
+
+  const renderers = {
+    listItem: (props: any): any => {
+      const { children } = props;
+      if (typeof props.checked === 'boolean') {
+        const { checked, sourcePosition } = props;
+        return (
+          <WrapCheckBox
+            markdown={value}
+            setMarkdown={setValue}
+            checked={checked}
+            sourcePosition={sourcePosition}>
+            {children}
+          </WrapCheckBox>
+        );
+      }
+      return <li>{children}</li>;
+    },
+    code: CodeBlock,
+  };
 
   return (
     <div className="note">
@@ -119,8 +166,9 @@ const NoteContainer: FC<Props> = ({ noteId }) => {
           {(typingMode === 'preview' || typingMode === 'both') && (
             <div className="react-markdown-container markdown-body">
               <ReactMarkdown
-                renderers={{ code: CodeBlock }}
+                renderers={renderers}
                 source={value}
+                rawSourcePos
                 escapeHtml={false}
               />
             </div>
