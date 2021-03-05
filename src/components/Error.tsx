e0fd723564860fbdcd21c983908e11e703882b91
MateuszRostkowski <mateusz.rostkowsky995@gmail.com>
2021-03-05 20:53:14 +0100

    edit error message

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Error = () => {
   return (
     <div className="error-container">
-      <h1>Nie znaleźono notatki</h1>
+      <h1>This note wasn't found</h1>
     </div>
   );
 };
