3f2c4927ee3de34ba722f7594470cedf5c96a7ca
MateuszRostkowski <mateusz.rostkowsky995@gmail.com>
2021-03-06 17:02:30 +0100

    refactor Sidebar components

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,19 +1,47 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { ListNoteItem } from '../helpers/interfaces';
 import AddNote from './AddNote';
 import { CSSTransition } from 'react-transition-group';
 import { useNotes } from '../hooks/useNotes';
 
+const SideBarNotesList = () => {
+  const { notes } = useNotes();
+  const { noteId } = useParams();
+
+  return (
+    <div className="links-container">
+      {notes.map((item: ListNoteItem, index: number) => {
+        return (
+          <Link
+            key={index}
+            to={`/${item.name}`}
+            className={noteId === item.name ? 'active-item' : ''}>
+            {item.name}
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
+
 function SideBar() {
   const [addMode, setAddMode] = useState(false);
-  const { notes } = useNotes();
   const [showNotesList, setShowNotesList] = useState(false);
   const sideBarRef = useRef<HTMLDivElement>(null);
 
-  const { noteId } = useParams();
+  const toggleMode = useCallback(() => {
+    setAddMode(state => !state);
+  }, []);
 
-  const toggleMode = () => setAddMode(!addMode);
+  const handleAddNoteClick = useCallback(
+    e => {
+      e.preventDefault();
+      toggleMode();
+    },
+    [toggleMode],
+  );
+
   return (
     <>
       <div
@@ -32,25 +60,11 @@ function SideBar() {
         <div className="sidebar-wrapper" ref={sideBarRef}>
           <div className="sidebar-container">
             <h1>Notes app</h1>
-            <a
-              href="/"
-              onClick={e => {
-                e.preventDefault();
-                toggleMode();
-              }}>
+            <a href="/" onClick={handleAddNoteClick}>
               Add note
             </a>
             <div className="links-container">
-              {notes.map((item: ListNoteItem, index: number) => {
-                return (
-                  <Link
-                    key={index}
-                    to={`/${item.name}`}
-                    className={noteId === item.name ? 'active-item' : ''}>
-                    {item.name}
-                  </Link>
-                );
-              })}
+              <SideBarNotesList />
             </div>
           </div>
         </div>
